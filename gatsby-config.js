60b5588581af10c0ddd4eb6b097f4108bbd3df81
lilpolymath <ayofavour7@gmail.com>
2020-06-04 20:42:19 +0100

    chore: google tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: 'UA-168581843-1',
       },
     },
     `gatsby-plugin-sitemap`,
